1f29970f360f2b54b55759fa8ed17ad5c5041186
salwaterind <73805540+salwaterind@users.noreply.github.com>
2020-11-03 12:34:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://water-indonesia.glitch.me"]
+const urls = ["https://confusion-victorious-windscreen.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
